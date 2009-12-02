df2094d18746ffad047755184f731cb3987550f4
Andrey Demidov <andrey.demidov@gmail.com>
2009-12-02 10:10:31 +0300

    read url lines

---
+++ settings.erl
@@ -0,0 +1,16 @@
+-module(settings).
+-export([read/0]).
+
+read()->
+    io:format("here~n"),
+    {ok, Device} = file:open("links", [read]),
+    get_lines_from_file(Device, []).
+
+get_lines_from_file(Device, Accum)->
+    case io:get_line(Device, "") of
+	eof->
+	    file:close(Device),
+	    Accum;
+	Line->
+	    get_lines_from_file(Device, Accum ++ [Line])
+    end.
