81e54d396a9d6fe00a9e3dbf02208a69f7d85295
Andrey Demidov <andrey.demidov@gmail.com>
2009-12-05 14:05:25 +0300

    refactore

--- web_crawler.erl
+++ web_crawler.erl
@@ -6,7 +6,7 @@ download_xml(Url)->
     ibrowse:start(),
     io:format("~p~n", [Url]),
     {ok, _StatusCode, _Headers, WebPageText} = ibrowse:send_req(Url, [], get),
-    io:format("~p~n", [WebPageText]),
-    xml_agregator:process_xml(WebPageText).
+    Result = xml_agregator:process_xml(WebPageText),
+    io:format("~p~n", [Result]).
 
 
--- xml_agregator.erl
+++ xml_agregator.erl
@@ -5,23 +5,25 @@
 fix_xml(Xml)->
     "<?xml version=\"1.0\" encoding=\"utf-8\"?>" ++ string:substr(Xml, 49, length(Xml) - 48).
 
-processDate([{_,_,_,_,_,_,_,_,D,_}|T])->
-    io:format("~p~n", [D]),
-    processDate(T);
-processDate([]) ->
-    io:format("end~n").
+zip([H1|T1], [H2|T2], Acc)->
+    zip(T1, T2, Acc ++ [{H1, H2}]);
+zip([], [], Acc) ->
+    Acc.
+    
+processDate([{_,_,_,_,_,_,_,_,D,_}|T], Acc)->
+    processDate(T, Acc ++ [D]);
+processDate([], Acc) ->
+    Acc.
 
-processValue([{_,_,_,_,V,_}|T])->
-    io:format("~p~n", [V]),
-    processValue(T);
-processValue([]) ->
-    io:format("end~n").
-
+processValue([{_,_,_,_,V,_}|T], Acc)->
+    processValue(T, Acc ++ [V]);
+processValue([], Acc) ->
+    Acc.
 
 process_xml(Xml)->
     {R, _} = xmerl_scan:string(fix_xml(Xml)),
-    processDate(xmerl_xpath:string("//@Date", R)),
-    processValue(xmerl_xpath:string("//Value/text()", R)).
+    zip(processDate(xmerl_xpath:string("//@Date", R), []), 
+	processValue(xmerl_xpath:string("//Value/text()", R), []), []).
     
     
 
