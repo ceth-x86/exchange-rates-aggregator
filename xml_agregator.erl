9efc285181b9604758ddf309efc4055134219de3
Andrey Demidov <andrey.demidov@gmail.com>
2009-12-05 14:29:51 +0300

    refactore: use standart lists:zip

--- xml_agregator.erl
+++ xml_agregator.erl
@@ -4,11 +4,6 @@
 
 fix_xml(Xml)->
     "<?xml version=\"1.0\" encoding=\"utf-8\"?>" ++ string:substr(Xml, 49, length(Xml) - 48).
-
-zip([H1|T1], [H2|T2], Acc)->
-    zip(T1, T2, Acc ++ [{H1, H2}]);
-zip([], [], Acc) ->
-    Acc.
     
 processDate([{_,_,_,_,_,_,_,_,D,_}|T], Acc)->
     processDate(T, Acc ++ [D]);
@@ -22,8 +17,8 @@ processValue([], Acc) ->
 
 process_xml(Xml)->
     {R, _} = xmerl_scan:string(fix_xml(Xml)),
-    zip(processDate(xmerl_xpath:string("//@Date", R), []), 
-	processValue(xmerl_xpath:string("//Value/text()", R), []), []).
+    lists:zip(processDate(xmerl_xpath:string("//@Date", R), []), 
+	processValue(xmerl_xpath:string("//Value/text()", R), [])).
     
     
 
